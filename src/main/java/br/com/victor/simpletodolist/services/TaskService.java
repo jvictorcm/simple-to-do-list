cce3a93dcfc8cb2d2ef190e76b81a924a49891bc
Victor <9091177+jvictorcm@users.noreply.github.com>
2023-01-26 17:17:27 +0000

    doc TaskService

--- src/main/java/br/com/victor/simpletodolist/services/TaskService.java
+++ src/main/java/br/com/victor/simpletodolist/services/TaskService.java
@@ -3,6 +3,7 @@ package br.com.victor.simpletodolist.services;
 import br.com.victor.simpletodolist.api.dtos.TaskInputDTO;
 import br.com.victor.simpletodolist.exceptions.TaskNotFoundException;
 import br.com.victor.simpletodolist.exceptions.TaskPutMappingCreation;
+import br.com.victor.simpletodolist.exceptions.TaskValidationException;
 import br.com.victor.simpletodolist.models.Task;
 import br.com.victor.simpletodolist.repositories.TaskRepository;
 import br.com.victor.simpletodolist.validators.TaskValidator;
@@ -15,6 +16,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * The class TaskService provides the service layer for the Task entity.
+ * It contains methods for creating, reading, updating and deleting tasks.
+ *
+ * @author jvictorcm
+ */
 @Service
 public class TaskService {
 
@@ -23,6 +30,13 @@ public class TaskService {
 
     Logger logger = LoggerFactory.getLogger(TaskService.class);
 
+    /**
+     * The findById method is used to find a task by id.
+     *
+     * @param id the id of the task
+     * @return Task the task found
+     * @throws TaskNotFoundException if the task is not found
+     */
     public Task findById(UUID id) {
         Optional<Task> resultObject = taskRepository.findById(id);
         if (resultObject.isPresent()) {
@@ -31,10 +45,22 @@ public class TaskService {
         throw new TaskNotFoundException(id);
     }
 
+    /**
+     * The deleteById method is used to delete a task by id.
+     *
+     * @param id the id of the task
+     */
     public void deleteById(UUID id) {
         taskRepository.deleteById(id);
     }
 
+    /**
+     * The createTask method is used to create a task.
+     *
+     * @param taskInputDTO the task input data
+     * @return Task the task created
+     * @throws TaskValidationException in case of an invalid task name
+     */
     public Task createTask(TaskInputDTO taskInputDTO) {
         TaskValidator.validateTaskName(taskInputDTO.getTask());
         Task newTask = new Task(taskInputDTO);
@@ -42,10 +68,24 @@ public class TaskService {
         return newTask;
     }
 
+    /**
+     * Returns a list of all Task objects
+     *
+     * @return A list of all Task objects
+     */
     public List<Task> listAllTasks() {
         return taskRepository.findAll();
     }
 
+    /**
+     * Update a task by id or create a new task whilst throws an Exception for that.
+     *
+     * @param taskInputDTO the task input dto
+     * @param id           the id
+     * @return Task the created task
+     * @throws TaskPutMappingCreation  in case of not finding a task with the provided id
+     * @throws TaskValidationException in case of an invalid task name
+     */
     public Task updateTask(TaskInputDTO taskInputDTO, UUID id) {
         TaskValidator.validateTaskName(taskInputDTO.getTask());
         Task resultObject = taskRepository.findById(id).map(task -> {
